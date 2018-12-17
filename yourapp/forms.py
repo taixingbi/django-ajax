f36cc5bd6c23b0043d132c5e88a590b08bac4644
taixingbi <bitaihang@gmail.com>
2018-12-16 19:01:28 -0500

    first commit

---
+++ yourapp/forms.py
@@ -0,0 +1,8 @@
+# yourapp.forms.py
+from django import forms
+
+class JoinForm(forms.Form): # or forms.ModelForm
+    email = forms.EmailField()
+    name = forms.CharField(max_length=120)
+
+    
---
+++ yourapp/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+from django.views.generic import FormView
+from .forms import JoinForm
+
+from .mixins import AjaxFormMixin
+
+
+class JoinFormView(AjaxFormMixin, FormView):
+    form_class = JoinForm
+    template_name  = 'forms/ajax.html'
+    success_url = '/form-success/'
